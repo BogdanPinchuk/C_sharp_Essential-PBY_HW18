5f59cd145f28af18f2ebc528f944c718c5016ad5
Bogdan <pinchuk.brus@gmail.com>
2019-08-12 08:28:16 +0300

    Add LesApp0

---
+++ LesApp0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Підключення іншого простору імен
+using MyNamespace;
+
+namespace LesApp0
+{
+    class Program
+    {
+        static void Main()
+        {
+            // перевірка доступу до класу із іншого простору імен
+            MyClass @class = new MyClass();
+
+            Console.WriteLine("Базовий простір імен: " + typeof(Program));
+            Console.WriteLine(@class.GetType());
+
+            // delay
+            Console.ReadKey(true);
+        }
+    }
+}
